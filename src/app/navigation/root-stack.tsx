64a60040ed1545e76d33585727f4a1e9a7ba22af
AgustinCarthery <agcarthery@gmail.com>
2024-02-01 16:13:21 -0300

    RF-48: navigation fixes

--- src/app/navigation/root-stack.tsx
+++ src/app/navigation/root-stack.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+// import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { RootStackParamList } from 'src/types/navigation';
@@ -12,32 +12,12 @@ export const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export const RootStackNavigator = () => {
   const [initializing, setInitializing] = useState(true);
-  const [isAuthenticated, setisAuthenticated] = useState<boolean>(false);
-
-  const onAuthStateChanged = useCallback(
-    async (firebaseUser: FirebaseAuthTypes.User | null) => {
-      if (initializing) setInitializing(false);
-
-      if (!firebaseUser) {
-        const token = await AsyncStorage.getItem('token');
-        setisAuthenticated(!!token);
-      }
-    },
-    [initializing],
-  );
-
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber;
-  }, [onAuthStateChanged]);
+  const [initialRoute, setInitialRoute] =
+    useState<keyof RootStackParamList>('Login');
 
   const checkToken = async () => {
-    try {
-      const token = await AsyncStorage.getItem('token');
-      setisAuthenticated(!!token);
-    } catch (error) {
-      setisAuthenticated(false);
-    }
+    const token = await AsyncStorage.getItem('token');
+    if (token) setInitialRoute('Main');
   };
 
   useEffect(() => {
@@ -52,17 +32,14 @@ export const RootStackNavigator = () => {
   if (initializing) return null;
 
   return (
-    <RootStack.Navigator>
+    <RootStack.Navigator initialRouteName={initialRoute}>
       {/* TODO : add onboarding stack */}
-      {isAuthenticated ? (
-        <RootStack.Screen
-          name="Main"
-          options={{ headerShown: false }}
-          component={MainAppNavigation}
-        />
-      ) : (
-        getAuthenticatedGroup()
-      )}
+      <RootStack.Screen
+        name="Main"
+        options={{ headerShown: false }}
+        component={MainAppNavigation}
+      />
+      {getAuthenticatedGroup()}
     </RootStack.Navigator>
   );
 };
