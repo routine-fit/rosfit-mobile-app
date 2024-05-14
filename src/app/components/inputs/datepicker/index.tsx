23e044ddc7554c3b55ba3906e34c48e7b054f7ef
AgustinCarthery <agcarthery@gmail.com>
2024-05-14 16:29:18 -0300

    RF-XX: update on datepicker component to use controlled input

--- src/app/components/inputs/datepicker/index.tsx
+++ src/app/components/inputs/datepicker/index.tsx
@@ -1,72 +1,56 @@
-import { useTheme } from 'styled-components/native';
-import React, { FC, useState } from 'react';
+import React, { useState } from 'react';
 import { FieldValues, useController } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { TextInput as RNTextInput } from 'react-native';
 import DatePicker from 'react-native-date-picker';
-import * as RNLocalize from 'react-native-localize';
 
-import Text from '../../text';
 import TextInput from '../text-input';
-import { Container } from '../text-input/styles';
 import { ControlledDatepickerProps } from './types';
 
-const ControlledDatepicker: FC<ControlledDatepickerProps<FieldValues>> = ({
-  controller,
-  formControlProps = {},
-}) => {
-  const { field, fieldState } = useController(controller);
+const ControlledDatepickerInner = <Form extends FieldValues>(
+  { controller, ...restOfProps }: ControlledDatepickerProps<Form>,
+  ref: React.RefObject<RNTextInput>,
+) => {
+  const {
+    field: { onChange, onBlur, value },
+    fieldState: { error },
+  } = useController(controller);
+
   const { t } = useTranslation();
-  const [showDatepicker, setShowDatepicker] = useState<boolean>(false);
-  const theme = useTheme();
 
   const label = t(`inputs:label.${controller.name}`);
   const placeholder = t(`inputs:placeholder.${controller.name}`);
-  const datepickerTitle = t(`inputs:placeholder.datepicker`);
+
+  const [showDatepicker, setShowDatepicker] = useState(false);
 
   return (
-    <Container
-      isInvalid={fieldState.invalid}
-      isRequired={formControlProps.isRequired || !!controller.rules?.required}
-      {...formControlProps}
-    >
-      <Text fontSize="xs" fontWeight="bold" color={theme.colors.content.subtle}>
-        {label}
-      </Text>
+    <>
       <TextInput
+        error={error?.message}
+        label={label}
         placeholder={placeholder}
-        value={
-          field.value &&
-          field.value.toDateString() === new Date().toDateString()
-            ? ''
-            : field.value?.toLocaleDateString() || ''
-        }
-        onFocus={() => setShowDatepicker(true)}
-        onBlur={() => setShowDatepicker(false)}
+        {...restOfProps}
+        onBlur={onBlur}
+        inputRef={ref}
+        value={value.toLocaleDateString()}
+        onPress={() => setShowDatepicker(true)}
       />
       <DatePicker
         modal
         open={showDatepicker}
-        date={field.value}
+        date={value}
         mode="date"
         maximumDate={new Date()}
-        title={datepickerTitle}
-        locale={RNLocalize.getLocales()[0].languageCode}
+        title={placeholder}
         onConfirm={(date: Date) => {
-          field.onChange(date);
+          onChange(date);
           setShowDatepicker(false);
         }}
       />
-      {fieldState.error && (
-        <Text
-          fontSize="xs"
-          color={theme.colors.feedback.error.default}
-          fontWeight="medium"
-        >
-          {fieldState.error.message}
-        </Text>
-      )}
-    </Container>
+    </>
   );
 };
 
-export default ControlledDatepicker;
+const ControlledDatePicker = React.forwardRef(ControlledDatepickerInner);
+
+export default ControlledDatePicker;
