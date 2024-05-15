883f019bb13b888c1830441fea8976d34262507b
AgustinCarthery <agcarthery@gmail.com>
2024-05-15 19:54:06 -0300

    RF-52: refactor ControlledSelectInput

---
+++ src/app/components/inputs/select/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const Overlay = styled.View`
+  flex: 1;
+  background-color: rgba(0, 0, 0, 0.5);
+  justify-content: flex-end;
+`;
+
+export const ModalContent = styled.View`
+  background-color: #fff;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  padding: 20px;
+  align-items: center;
+`;
+
+export const Option = styled.View`
+  padding: 8px;
+`;
