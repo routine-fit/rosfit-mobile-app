e0d8a2a3482180ad0ae99b9af43257d187696816
Franco Marini <franco-marini@outlook.com>
2024-09-25 15:30:22 -0300

    RF-42: fix handle token expired

--- src/api/rosfit.api.ts
+++ src/api/rosfit.api.ts
@@ -27,26 +27,28 @@ rosFitApi.interceptors.response.use(
       `${error?.request?.responseURL}, ${error?.response?.status}`,
       JSON.stringify(error?.response?.data, undefined, 2),
     );
-
-    if (error.response?.data?.data?.type === ApiCodes.TOKEN_EXPIRED) {
-      const { config } = error;
-      const user = firebaseAuth.currentUser;
-      if (user && config) {
-        const token = await user.getIdToken(true);
-        config.headers.Authorization = token;
-        await AsyncStorage.setItem('token', token);
-        return new Promise((resolve, reject) => {
-          rosFitApi.request(config).then(resolve).catch(reject);
-        });
+    if (error.response?.status === 401) {
+      if (error.response?.data?.data?.type === ApiCodes.TOKEN_EXPIRED) {
+        const { config } = error;
+        const user = firebaseAuth.currentUser;
+        if (user && config) {
+          const token = await user.getIdToken(true);
+          config.headers.Authorization = token;
+          await AsyncStorage.setItem('token', token);
+          return new Promise((resolve, reject) => {
+            axios.request(config).then(resolve).catch(reject);
+          });
+        }
       }
+
+      const navigation = getNavigationRef();
+      navigation?.reset({
+        index: 0,
+        routes: [{ name: 'Login' }],
+      });
+      await AsyncStorage.setItem('token', '');
     }
 
-    const navigation = getNavigationRef();
-    navigation?.reset({
-      index: 0,
-      routes: [{ name: 'Login' }],
-    });
-    await AsyncStorage.setItem('token', '');
     return Promise.reject(error);
   },
 );
